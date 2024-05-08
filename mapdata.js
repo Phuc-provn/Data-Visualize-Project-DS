9c3cb7305d99ca6bf79ff1b15e98f2c54579f9e2
Phuc-provn <106170382+Phuc-provn@users.noreply.github.com>
2024-05-08 18:34:23 +0700

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,338 @@
+var simplemaps_europemap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "detect",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "diamond",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    arrow_color: "#cecece",
+    arrow_color_border: "#808080",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    AL: {
+      color: "#a4bcda",
+      name: "Albania",
+      description: "$7,492,735"
+    },
+    AM: {
+      color: "#a4bcda",
+      name: "Armenia",
+      description: " "
+    },
+    AT: {
+      color: "#436da2",
+      name: "Austria",
+      description: " "
+    },
+    BA: {
+      color: "#a4bcda",
+      name: "Bosnia and Herzegovina",
+      description: " "
+    },
+    BE: {
+      color: "#436da2",
+      name: "Belgium",
+      description: "$9,983,321"
+    },
+    BG: {
+      color: "#80a1cb",
+      name: "Bulgaria",
+      description: "$587,986"
+    },
+    BY: {
+      color: "#80a1cb",
+      name: "Belarus",
+      description: " "
+    },
+    CH: {
+      color: "#a4bcda",
+      name: "Switzerland",
+      description: " "
+    },
+    CY: {
+      color: "#a4bcda",
+      name: "Cyprus",
+      description: " "
+    },
+    CZ: {
+      color: "#436da2",
+      name: "Czech Republic",
+      description: "$5,599,132"
+    },
+    DE: {
+      color: "#34547e",
+      name: "Germany",
+      description: "$78,042,939"
+    },
+    DK: {
+      color: "#436da2",
+      name: "Denmark",
+      description: "$11,179,770"
+    },
+    EE: {
+      color: "#a4bcda",
+      name: "Estonia",
+      description: "$410,336"
+    },
+    ES: {
+      color: "#34547e",
+      name: "Spain",
+      description: "$21,887,869"
+    },
+    FI: {
+      color: "#5c86bc",
+      name: "Finland",
+      description: "$5,308,968"
+    },
+    FR: {
+      color: "#34547e",
+      name: "France",
+      description: "$67,311,500"
+    },
+    GB: {
+      color: "#34547e",
+      name: "United Kingdom",
+      description: "$117,228,296"
+      
+    },
+    GE: {
+      color: "#a4bcda",
+      name: "Georgia",
+      description: "$3,350,025"
+    },
+    GR: {
+      color: "#5c86bc",
+      name: "Greece",
+      description: "$4,186,117"
+    },
+    HR: {
+      color: "#80a1cb",
+      name: "Croatia",
+      description: "$840,797"
+    },
+    HU: {
+      color: "#5c86bc",
+      name: "Hungary",
+      description: "$3,379,099"
+    },
+    IE: {
+      color: "#34547e",
+      name: "Ireland",
+      description: "$549,712"
+    },
+    IS: {
+      color: "#a4bcda",
+      name: "Iceland",
+      description: "$931,199"
+    },
+    IT: {
+      color: "#34547e",
+      name: "Italy",
+      description: "$29,775,040"
+    },
+    LT: {
+      color: "#a4bcda",
+      name: "Lithuania",
+      description: "$357,092"
+    },
+    LU: {
+      color: "#a4bcda",
+      name: "Luxembourg",
+      description: "$2,995,140"
+    },
+    LV: {
+      color: "#a4bcda",
+      name: "Latvia",
+      description: "$276,038"
+    },
+    MD: {
+      color: "#80a1cb",
+      name: "Moldova",
+      description: "$110,532 "
+    },
+    ME: {
+      color: "#a4bcda",
+      name: "Montenegro",
+      description: "$287,899"
+    },
+    MK: {
+      color: "#a4bcda",
+      name: "Macedonia",
+      description: "$110,532 "
+    },
+    NL: {
+      color: "#34547e",
+      name: "Netherlands",
+      description: "$17,236,734"
+    },
+    NO: {
+      color: "#436da2",
+      name: "Norway",
+      description: "$11,261,743"
+    },
+    PL: {
+      color: "#436da2",
+      name: "Poland",
+      description: "$8,895,199"
+    },
+    PT: {
+      color: "#5c86bc",
+      name: "Portugal",
+      description: "$3,859,876"
+    },
+    RO: {
+      color: "#80a1cb",
+      name: "Romania",
+      description: "$944,566"
+    },
+    RS: {
+      color: "#a4bcda",
+      name: "Serbia",
+      description: "$287,899"
+    },
+    SE: {
+      color: "#34547e",
+      name: "Sweden",
+      description: "$15,899,826"
+    },
+    SI: {
+      color: "#80a1cb",
+      name: "Slovenia",
+      description: "$616,381"
+    },
+    SK: {
+      color: "#80a1cb",
+      name: "Slovakia",
+      description: "$1,687,087"
+    },
+    TR: {
+      color: "#5c86bc",
+      name: "Turkey",
+      description: "$4,302,719"
+    },
+    UA: {
+      color: "#5c86bc",
+      name: "Ukraine",
+      description: "$3,558,720"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "51.509865",
+      lng: "-0.136439",
+      name: "London",
+      color: "red"
+    }
+  },
+  regions: {},
+  labels: {},
+  legend: {
+    entries: []
+  },
+  data: {
+    data: {
+      LV: "1",
+      RS: "2",
+      LT: "3",
+      EE: "4",
+      IS: "6",
+      BG: "7",
+      SI: "8",
+      HR: "10",
+      RO: "11",
+      SK: "12",
+      HU: "13",
+      UA: "14",
+      PT: "15",
+      GR: "16",
+      TR: "17",
+      FI: "18",
+      CZ: "19",
+      AT: "20",
+      PL: "21",
+      BE: "22",
+      DK: "23",
+      NO: "24",
+      SE: "25",
+      NL: "26",
+      ES: "27",
+      IT: "28",
+      FR: "29",
+      DE: "30",
+      GB: "31",
+      ME: "2",
+      IE: "31",
+      MK: "5",
+      GE: "5",
+      CY: "2",
+      LU: "1",
+      CH: "2",
+      AM: "4",
+      BY: "11",
+      BA: "3",
+      AL: "6",
+      MD: "8"
+    }
+  }
+};
